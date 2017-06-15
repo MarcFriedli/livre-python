199ba0f20689bf68de2671ab284d510a62dce837
marc.friedli <marc.friedli@he-arc.ch>
2017-06-15 12:46:17 +0200

    Correctif : oubli de guillemets.

--- source/dunders/name_dunder.py
+++ source/dunders/name_dunder.py
@@ -6,7 +6,7 @@ def some_function():
     print("I'm a script")
 
 
-if __name__ == __main__:
+if __name__ == '__main__':
     """
         some_function ne s'exécute que si on lance directement le module
         et pas si on l'importe.
